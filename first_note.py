e1dcdb4f549da6e263f149590c97a8107a54a712
sssctkt <justaskaisssctkt@gmail.com>
2026-02-02 09:13:20 -0500

    first file

---
+++ first_note.py
@@ -0,0 +1,50 @@
+#if you can save into onedrive, go to onedrive-->documents
+#save your file as "first_note"
+
+#if you cannot save into onedrive, go to documents
+#save your file as "first_note"
+
+#let's begin
+#what is programming? Programming is more like a platform between
+#human and computer
+
+#human gives instruction to computer in the form of programming, and computer
+#will execute the instruction
+
+#what does programming-->change from human command to computer instruction
+
+#we have many many programming language--java, C++, assembly language, python
+
+#some programming languages are called "low level" programming language
+#example-->assembly language
+#harder to understand by human, but easier to be understood by computer
+
+#some programming languages are called "high level" programming language
+#example-->python
+#needs more translation so computer can understand it, but easier to be understood
+#from human
+
+#next part
+#where do we write our program on?
+#we write it on IDE--integrated development environment
+#it helps your programming experience to be much smoother
+
+# # is a speical comment, it tells the computer that this is a comment
+# and disregard it
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

9de77de6237e2f38de021112544480ae41dea0bc
sssctkt <justaskaisssctkt@gmail.com>
2026-02-03 10:25:47 -0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,40 @@
+#goals of today: you can create a very first simple python program
+
+#first instruction--how to print
+
+print("Welcome to ICD2O0")
+print("This is a very good class, except ppl are exchanging pink slip")
+print("Now they are doing paper folding??")
+print("Where do you think this sentence will be placed")
+
+#each print statement will print in a new line
+
+#try to do the following
+#1. try to print the following message -->to be or not to be, that is the question
+
+#2-->"" is a double quotation, and '' is a single quotation
+#introuce the special character \ (backlash)
+print("\"\"\"")
+print("\"\" is a double quotation, and '' is a single quotation")
+#so....how to print the actual \
+print("\\")
+print("\"\"\"''\\\\\"")
+#answer..."""''\\"
+print("whatdo youthink it does")
+print("what\ndo you\tthink it does")
+#\"...print quotation
+#\\...print \
+#\n...newline
+#\t...tab
+
+
+
+
+
+
+
+
+
+
+
+

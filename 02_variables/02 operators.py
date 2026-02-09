5c8e976165ddc2b6d9f3a007a8dd863b958146ae
sssctkt <justaskaisssctkt@gmail.com>
2026-02-09 10:40:54 -0500

    Add files via upload

---
+++ 02_variables/02 operators.py
@@ -0,0 +1,29 @@
+#operators
+
+#variables--cannot start with a number, cannot have space in between
+#no symbols, cannot clash with keyword
+
+#assignment symbol, = assign the value to the variable right to left
+
+#operators (math, comparision, logical)
+
+#math operators +, -, *, /, **(exponent)
+
+print(2+3*5) #bedmas
+print(8*12)
+print("8"*12)
+print("karch"*3)
+print(3*2**2) #bedmas
+
+#special math operators
+#// it divides the number, then round DOWN
+#% 
+
+
+
+
+
+
+
+
+

42eb86d3b94f462c0cc7034e2d16cc28471b1f63
sssctkt <justaskaisssctkt@gmail.com>
2026-02-09 10:40:36 -0500

    Create variables.py

---
+++ 02_variables/variables.py
@@ -0,0 +1,19 @@
+#variables-symbol(s) that can store and manipulate values
+#there are rules in assigning variables
+# = (assignment symbol) assign values from RIGHT TO LEFT
+
+kemari="hard working student" #message you need quotation
+kwan=41 #numbers you do not need quotation
+print(kemari, kwan)
+
+x=25
+
+#how to create a variable
+
+two_phrases="python" #they can be connected by _
+#2x=121 #cannot start with number
+x2=121
+print(x2)
+#cannot have symbol (except _)
+#x@="python"
+#print=123 Do not use keyword as your variable name

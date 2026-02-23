5ecb95d3503f358aed44e9cf5a7e815c0c1bf547
sssctkt <justaskaisssctkt@gmail.com>
2026-02-23 09:06:10 -0500

    Add files via upload

---
+++ 02_variables/01 variables.py
@@ -0,0 +1,38 @@
+#variables-symbol(s) that can store and manipulate values
+#there are rules in assigning variables
+# = (assignment symbol) assign values from RIGHT TO LEFT
+
+kemari="hard working student" #message you need quotation
+kwan=41 #numbers you do not need quotation
+print(kemari, kwan)
+
+x=25
+
+#how to create a variable
+
+two_phrases="python" #they can be connected by _
+#2x=121 #cannot start with number
+x2=121
+print(x2)
+#cannot have symbol (except _)
+#x@="python"
+#print=123 Do not use keyword as your variable name
+
+
+#what is a good way in naming a variable
+x123=45
+#the variable should be more descrptive
+radius=45
+
+
+#mini task for today:
+#1. create 3 variable names to store number and message
+#2. create 3 variable names that won't work (better to find out why it
+#doesn't work)
+
+
+
+
+
+
+
---
+++ 02_variables/more operators.py
@@ -0,0 +1,34 @@
+#more operators!!
+
+#math operators
+#+ - * / // % **
+
+#assignment operators
+#= assign value from right to left
+#+=, -=, *= /=
+
+#comparison operators
+#> greater
+#< smaller
+#>= greater than or equal to
+#<= smaller than or equal to
+#== equal
+#!= not equal
+
+#logical operators
+#not--not true is false, not false is true
+#or --if at least one item is true, the whole expression is true
+#and --the whole expression is true if and only if every condition is true
+
+p=False
+q=False
+answer=((p and q) or p )
+print(answer)
+
+
+
+
+
+
+
+
